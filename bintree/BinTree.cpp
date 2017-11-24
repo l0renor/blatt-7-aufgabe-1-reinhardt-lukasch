1a95003b177144eb78b336b2f17d0f691d2956e7
Oliver Braun <ob@cs.hm.edu>
2017-11-24 15:15:38 +0100

    Startercode

---
+++ bintree/BinTree.cpp
@@ -0,0 +1,133 @@
+#include "BinTree.h"
+
+using namespace ::std;
+
+/********************************************************************
+ * Search
+ *******************************************************************/
+
+bool BinTree::search(const int value) const {
+
+}
+
+/********************************************************************
+ * Insert
+ *******************************************************************/
+
+void BinTree::insert(const int value) {
+
+}
+
+/********************************************************************
+ * Remove
+ *******************************************************************/
+
+void BinTree::remove(const int value) {
+
+}
+
+/********************************************************************
+ * Traversal
+ *******************************************************************/
+
+vector<int> *BinTree::preorder() const {
+    if (root == nullptr)
+        return nullptr;
+    return root->preorder();
+}
+
+vector<int> *BinTree::Node::preorder() const {
+    auto vec = new vector<int>();
+    // Wurzel in vec
+    vec->push_back(key);
+    // linken Nachfolger in vec
+    if (left != nullptr) {
+        auto left_vec = left->preorder();
+        vec->insert(vec->end(), left_vec->begin(), left_vec->end());
+    }
+    // rechten Nachfolger in vec
+    if (right != nullptr) {
+        auto right_vec = right->preorder();
+        vec->insert(vec->end(), right_vec->begin(), right_vec->end());
+    }
+    return vec;
+}
+
+vector<int> *BinTree::inorder() const {
+    if (root == nullptr)
+        return nullptr;
+    return root->inorder();
+}
+
+vector<int> *BinTree::Node::inorder() const {
+    auto vec = new vector<int>();
+    // linken Nachfolger in vec
+    if (left != nullptr) {
+        auto left_vec = left->inorder();
+        vec->insert(vec->end(), left_vec->begin(), left_vec->end());
+    }
+    // Wurzel in vec
+    vec->push_back(key);
+    // rechten Nachfolger in vec
+    if (right != nullptr) {
+        auto right_vec = right->inorder();
+        vec->insert(vec->end(), right_vec->begin(), right_vec->end());
+    }
+    return vec;
+}
+
+vector<int> *BinTree::postorder() const {
+    if (root == nullptr)
+        return nullptr;
+    return root->postorder();
+}
+
+vector<int> *BinTree::Node::postorder() const {
+    auto vec = new vector<int>();
+    // linken Nachfolger in vec
+    if (left != nullptr) {
+        auto left_vec = left->postorder();
+        vec->insert(vec->end(), left_vec->begin(), left_vec->end());
+    }
+    // rechten Nachfolger in vec
+    if (right != nullptr) {
+        auto right_vec = right->postorder();
+        vec->insert(vec->end(), right_vec->begin(), right_vec->end());
+    }
+    // Wurzel in vec
+    vec->push_back(key);
+    return vec;
+}
+
+/********************************************************************
+ * operator<<
+ *******************************************************************/
+std::ostream &operator<<(std::ostream &os, const BinTree &tree) {
+    function<void(std::ostream &, const int, const BinTree::Node *, const string)> printToOs
+            = [&](std::ostream &os, const int value, const BinTree::Node *node, const string l) {
+
+                static int nullcount = 0;
+
+                if (node == nullptr) {
+                    os << "    null" << nullcount << "[shape=point];" << endl;
+                    os << "    " << value << " -> null" << nullcount
+                       << " [label=\"" << l << "\"];" << endl;
+                    nullcount++;
+                } else {
+                    os << "    " << value << " -> " << node->key
+                       << " [label=\"" << l << "\"];" << endl;
+
+                    printToOs(os, node->key, node->left, "l");
+                    printToOs(os, node->key, node->right, "r");
+                }
+            };
+    os << "digraph tree {" << endl;
+    if (tree.root == nullptr) {
+        os << "    null " << "[shape=point];" << endl;
+    } else {
+        printToOs(os, tree.root->key, tree.root->left, "l");
+        printToOs(os, tree.root->key, tree.root->right, "r");
+    }
+    os << "}" << endl;
+    return os;
+}
